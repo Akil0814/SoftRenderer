6b44342b31e081e6225270090869b91ad254e4b5
Akil0814 <akil040814@gmail.com>
2026-04-23 17:43:35 -0700

    4/23

--- MAI_SGL/shader/texture_shader_2d.cpp
+++ MAI_SGL/shader/texture_shader_2d.cpp
@@ -11,12 +11,29 @@ namespace mai
 		const std::map<uint32_t, BufferObject*>& buffer_map,
 		size_t index)
 	{
-		(void)binding_map;
-		(void)buffer_map;
-		(void)index;
+	VsOutput output;
 
-		// TODO: Implement 2D vertex transformation and attribute extraction.
-		return VsOutput{};
+	vec4f position = get_vector(binding_map, buffer_map, 0, index);
+	position.z = 0.0f;
+	position.w = 1.0f;
+
+	vec4f color{ 1.0f, 1.0f, 1.0f, 1.0f };
+	if (has_attribute(binding_map, 1))
+	{
+		color = get_vector(binding_map, buffer_map, 1, index);
+	}
+
+	vec4f UV{ 0.0f, 0.0f, 0.0f, 0.0f };
+	if (has_attribute(binding_map, 2))
+	{
+		UV = get_vector(binding_map, buffer_map, 2, index);
+	}
+
+	output._position = _transform_matrix * position;
+	output._color = color;
+	output._UV = UV;
+
+	return output;
 	}
 
 	void TextureShader2D::fragment_shader(
@@ -24,11 +41,21 @@ namespace mai
 		FsOutput& output,
 		const std::map<uint32_t, Texture*>& textures)
 	{
-		(void)input;
-		(void)textures;
+		output._pixel_pos.x = static_cast<int>(input._position.x);
+		output._pixel_pos.y = static_cast<int>(input._position.y);
+		output._depth = input._position.z;
 
-		// TODO: Implement 2D texture sampling and pixel output.
-		output = FsOutput{};
+		auto iter = textures.find(_diffuse_texture);
+		if (iter == textures.end() || iter->second == nullptr)
+		{
+			output._color = vector_to_RGBA(input._color);
+			return;
+		}
+
+		Texture* texture = iter->second;
+
+		mai::vec4f diffuseColor = texture->get_color(input._UV.x, input._UV.y);
+		output._color = vector_to_RGBA(diffuseColor);
 	}
 
 }
